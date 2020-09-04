52c6e88a562457aa794275ee9b4245ffff827b37
aiyana <xinyih@andrew.cmu.edu>
2020-09-04 11:14:44 -0700

    Partner commiting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public static void main(String[] args) {
     System.out.println("Hi! Im Aashai!");
     System.out.println("Hello-world");
-    System.out.println("I have been at CMU for 2 years!")
+    System.out.println("I have been at CMU for 2 years!");
+    System.out.println("Hi I'm Aashai's partner Aiyana");
 }
